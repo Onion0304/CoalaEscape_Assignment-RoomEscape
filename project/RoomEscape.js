37f62a30e4cefc3878f416b64c60367753029dc6
Onion0304 <ljkp1638@naver.com>
2019-10-23 09:10:19 +0900

    2

--- project/RoomEscape.js
+++ project/RoomEscape.js
@@ -1,186 +1,360 @@
-room = game.createRoom("room", "배경-1.png") // 방 생성
-room2 = game.createRoom("room2", "배경-6.png") // 방2 생성
-
-room.door = room.createObject("door", "문-오른쪽-닫힘.png") // 문 생성
-room.door.setWidth(136) // 크기 조절
-room.locateObject(room.door, 1049, 300) // 문 배치
-room.door.lock() // door를 locked 상태로 변경
-
-room.door.onClick = function() { // door를 클릭했을 때
-	if(room.door.isClosed()){ // door가 closed 상태이면
-		room.door.open() // door를 open
-	} else if (room.door.isOpened()){ // door가 opened 상태이면
-		game.move(room2) // room2로 이동
-	} else if (room.door.isLocked()){ // door가 locked 상태이면
-		printMessage("문이 잠겨있다") // 잠금 메시지 출력
-	}
+Function.prototype.member = function(name, value){
+	this.prototype[name] = value
 }
 
-room.door.onOpen = function() { // door가 open 상태로 변경되면 실행
-	room.door.setSprite("문-오른쪽-열림.png") // 열린 문으로 변경
-}
-
-room.keypad = room.createObject("keypad", "숫자키-우.png") // 오브젝트 생성
-room.keypad.setWidth(50) // 크기 조절
-room.locateObject(room.keypad, 930, 250) // 위치 변경
-
-room.keypad.onClick = function() {
-	printMessage("올해는 몇 년도?")
-	showKeypad("number", "2019" , function(){ 
-		room.door.unlock() // door의 잠금을 연다
-		printMessage("잠금장치가 열리는 소리가 들렸다.") // 잠금 해제 메시지 출력
-	 })
-}
-
-room.shelf = room.createObject("shelf", "선반-좌.png")
-room.shelf.setWidth(460)
-room.locateObject(room.shelf, 250, 150)
-
-room.book = room.createObject("book", "책3-1.png")
-room.book.setWidth(80)
-room.locateObject(room.book, 100, 140)
-room.book.onClick = function(){
-    showImageViewer("종이.png", "책.txt"); // 이미지와 텍스트 파일 출력
-}
-
-room.phone = room.createObject("phone", "전화기-오른쪽.png")
-room.phone.setWidth(30)
-room.locateObject(room.phone, 830, 250)
-room.phone.onClick = function(){
-    playSound("alarm.wav") // 오디오 재생
-}
-
-room.radio = room.createObject("radio", "라디오.png")
-room.radio.setWidth(90)
-room.locateObject(room.radio, 100, 550)
-room.radio.onClick = function(){
-    printMessage("재생버튼을 눌러보세요")
-    showAudioPlayer("chick.wav") // 플레이어
-}
-
-room.table = room.createObject("table", "테이블-우.png")
-room.table.setWidth(300)
-room.locateObject(room.table, 550, 400)
-
-room.mac = room.createObject("mac", "맥-우.png")
-room.mac.setWidth(130)
-room.locateObject(room.mac, 560, 270)
-room.mac.onClick = function(){
-    showVideoPlayer("Wildlife.wmv") // 비디오 재생
-}
-
-room2.door = room2.createObject("door", "문-오른쪽-열림.png") // 문 생성
-room2.door.setWidth(136) // 크기 조절
-room2.locateObject(room2.door, 1049, 305) // 문 배치
-room2.door.open() // door를 open 상태로 변경
-room2.door.onClick = function(){
-	game.move(room) // room으로 이동
-}
-
-room2.cupboard = room2.createObject("cupboard", "찬장-2-닫힘.png") // 찬장 생성
-room2.key = room2.createObject("key", "열쇠.png") // 열쇠 생성
-room2.key.hide() // key 숨기기
-
-// 찬장, 열쇠 크기 조절 
-room2.cupboard.setWidth(250)
-room2.key.setWidth(45)
-
-// 찬장, 열쇠 배치
-room2.locateObject(room2.cupboard, 800, 323)
-room2.locateObject(room2.key, 745, 315)
-
-// 찬장, 열쇠 상태 변화
-room2.cupboard.onOpen = function(){ // 찬장이 open 상태일 때
-	room2.cupboard.setSprite("찬장-2-열림.png") // 열린 그림으로 변경
-	room2.key.show() // key 보이기 
-}
-room2.cupboard.onClose = function(){ // 찬장이 close 상태일 때
-	room2.cupboard.setSprite("찬장-2-닫힘.png") // 닫힌 그림으로 변경
-	room2.key.hide() // key 숨기기 
-}
-
-room2.cupboard.onClick = function(){ // 찬장을 클릭했을 때
-	if(room2.cupboard.isOpened()){ // 찬장이 opened 상태이면
-		room2.cupboard.close() // 찬장을 close 
-	} else if(room2.cupboard.isClosed()){ // 찬장이 closed 상태이면
-		room2.cupboard.open() // 찬장을 open 
+//**// Game Definition
+function Game(){}
+Game.start = function(room, welcome){
+	game.start(room.id)
+	if(welcome !== undefined){
+		game.printStory(welcome)
 	} else{
-		// 아무것도 하지 않는다
+		//nothing
+	}
+}
+Game.end = function(){
+	game.clear()
+}
+Game.move = function(room){
+	game.move(room.id)	
+}
+Game.handItem = function(){
+	return game.getHandItem()
+}
+
+//**// Room Definition
+function Room(name, background){
+	this.name = name
+	this.background = background
+	this.id = game.createRoom(name, background)
+}
+Room.member('setRoomLight', function(intensity){
+	this.id.setRoomLight(intensity)
+})
+
+//**// Object Definition
+function Object(room, name, image){
+	this.room = room
+	this.name = name
+	this.image = image
+
+	if (room !== undefined){
+		this.id = room.id.createObject(name, image)
+	}
+}
+Object.STATUS = { OPENED: 0, CLOSED: 1, LOCKED: 2 }
+
+Object.member('setSprite', function(image){
+	this.image = image
+	this.id.setSprite(image)
+})
+Object.member('resize', function(width){
+	this.id.setWidth(width)
+})
+Object.member('setDescription', function(description){
+	this.id.setItemDescription(description)
+})
+
+Object.member('getX', function(){
+	return this.id.getX()
+})
+Object.member('getY', function(){
+	return this.id.getY()
+})
+Object.member('locate', function(x, y){
+	this.room.id.locateObject(this.id, x, y)
+})
+Object.member('move', function(x, y){
+	this.id.moveX(x)
+	this.id.moveY(y)
+})
+
+Object.member('show', function(){
+	this.id.show()
+})
+Object.member('hide', function(){
+	this.id.hide()
+})
+Object.member('open', function(){
+	this.id.open()
+})
+Object.member('close', function(){
+	this.id.close()
+})
+Object.member('lock', function(){
+	this.id.lock()
+})
+Object.member('unlock', function(){
+	this.id.unlock()
+})
+Object.member('isOpened', function(){
+	return this.id.isOpened()
+})
+Object.member('isClosed', function(){
+	return this.id.isClosed()
+})
+Object.member('isLocked', function(){
+	return this.id.isLocked()
+})
+Object.member('pick', function(){
+	this.id.pick()
+})
+Object.member('isPicked', function(){
+	return this.id.isPicked()
+})
+
+//**// Door Definition
+function Door(room, name, closedImage, openedImage, connectedTo){
+	Object.call(this, room, name, closedImage)
+
+	// Door properties
+	this.closedImage = closedImage
+	this.openedImage = openedImage
+	this.connectedTo = connectedTo
+}
+// inherited from Object
+Door.prototype = new Object()
+
+Door.member('onClick', function(){
+	if (!this.id.isLocked() && this.id.isClosed()){
+		this.id.open()
+	}
+	else if (this.id.isOpened()){
+		if (this.connectedTo !== undefined){
+			Game.move(this.connectedTo)
+		}
+		else {
+			Game.end()
+		}
+	}
+})
+Door.member('onOpen', function(){
+	this.id.setSprite(this.openedImage)
+})
+Door.member('onClose', function(){
+	this.id.setSprite(this.closedImage)
+})
+
+//**// Keypad Definition
+function Keypad(room, name, image, password, callback){
+	Object.call(this, room, name, image)
+
+	// Keypad properties
+	this.password = password
+	this.callback = callback
+}
+// inherited from Object
+Keypad.prototype = new Object()
+
+Keypad.member('onClick', function(){
+	showKeypad('number', this.password, this.callback)
+})
+
+//**// DoorLock Definition
+function DoorLock(room, name, image, password, door, message){
+	Keypad.call(this, room, name, image, password, function(){
+		printMessage(message)
+		door.unlock()
+	})
+}
+// inherited from Object
+DoorLock.prototype = new Keypad()
+
+//**// Computer Definition
+function Computer(room, name, image, message){
+	Object.call(this, room, name, image)
+
+    // computer properties
+	this.message = message
+}
+
+Computer.prototype = new Object
+
+Computer.member('onClick', function(){
+	printMessage(this.message)
+})
+
+//**// Direction Definition
+function Direction(room, name, Image, connectedTo){
+	Object.call(this, room, name, Image)
+
+	this.Image = Image
+	this.connectedTo = connectedTo
+}
+
+Direction.prototype = new Object()
+
+Direction.member('onClick', function(){
+	Game.move(this.connectedTo)
+})
+
+//**// openclose Definition
+function openclose(room, name, closedImage, openedImage){
+	Object.call(this, room, name, closedImage)
+
+    // openclose properties
+	this.closedImage = closedImage
+	this.openedImage = openedImage
+}
+
+openclose.prototype = new Object()
+
+openclose.member('onClick', function(){
+	if (!this.id.isLocked() && this.id.isClosed()){
+		this.id.open()
+	} else if (this.id.isOpened){
+		this.id.close()
+	}
+})
+openclose.member('onOpen', function(){
+	this.id.setSprite(this.openedImage)
+})
+openclose.member('onClose', function(){
+	this.id.setSprite(this.closedImage)
+})
+
+//**// Item Definition
+function Item(room, name, image){
+	Object.call(this, room, name, image)
+}
+// inherited from Object
+Item.prototype = new Object()
+
+Item.member('onClick', function(){
+	this.id.pick()
+})
+Item.member('isHanded', function(){
+	return Game.handItem() == this.id
+})
+
+
+////****//// 방 생성
+room1 = new Room('room1', '배경-1.png')		
+room2 = new Room('room2', '배경-4.png')		
+room3 = new Room('room3', '배경-3.png')		
+storage = new Room('storage', '창고.jpg')
+lab = new Room('lab', '실험실.jpg')
+
+////*** 첫번째 방 ***////
+room1.door = new Door(room1, 'door', '문-오른쪽-닫힘.png', '문-오른쪽-열림.png', room2)
+room1.door.resize(120)
+room1.door.locate(1000, 312)
+room1.door.lock()
+room1.door.onClick = function(){
+	if(room1.keypad.isLocked()){
+		printMessage('잠겨있다')
+	} else if(!room1.keypad.isLocked()){
+		Game.move(this.connectedTo)
+		playSound('무전3.wav')
+		printMessage('잘하셨습니다\n계속 진행해주세요')
 	}
 }
 
-room.head = room.createObject("head", "드라이버비트.png")
-room.handle = room.createObject("handle", "드라이버손잡이.png")
-room.screwdriver = room.createObject("screwdriver", "드라이버.png")
+playSound('무전3.wav') 
 
-// 드라이버, 비트, 손잡이 크기 조절
-room.head.setWidth(50)
-room.handle.setWidth(50)
-room.screwdriver.hide() // 조합 될 아이템 숨기기
+room1.keypad = new DoorLock(room1, 'keypad', '숫자키-우.png', '0000', room1.door, '문이 열렸다') // 키패드
+room1.keypad.resize(50) // 키패드 사이즈
+room1.keypad.locate(1100, 310) // 키패드 위치 변경
 
-// 드라이버 비트, 손잡이 배치
-room.locateObject(room.head, 500, 650)
-room.locateObject(room.handle, 600, 650)
+room1.table = new Object(room1, 'table', '테이블-오른쪽.png')
+room1.table.resize(300)
+room1.table.locate(550, 400)
 
-room.head.onClick = function(){
-	room.head.pick() // 줍기 
+room1.twradio = new Item(room1, 'twradio', '무전기.jpg')
+room1.twradio.resize(40)
+room1.twradio.locate(560, 270)
+room1.twradio.onClick = function(){
+	playSound('무전3.wav')
+	printMessage('지금부터 그곳에서 탈출하셔야 합니다 건투를 빕니다')
+	this.id.pick()
 }
-room.handle.onClick = function(){
-	room.handle.pick()
-}
-game.makeCombination(room.head, room.handle, room.screwdriver) // 헤드 + 손잡이 = 드라이버
 
-room.shelf.onClick = function(){
-	if(game.getHandItem() == room.screwdriver){ // 드라이버를 들고 있으면
-		printMessage("드라이버로 나사를 단단히 조였다")
-	} else{
-		printMessage("나사가 헐거워져있다")
+room1.key = new Item(room1, 'key', '열쇠.png')
+room1.key.resize(45)
+room1.key.locate(745, 500)
+
+
+////*** 두번째 방 ***////
+room2.door1 = new Door(room2, 'door1', '문-왼쪽-닫힘.png', '문-왼쪽-열림.png', room1)
+room2.door1.resize(120)
+room2.door1.locate(300, 270)
+
+room2.door2 = new Door(room2, 'door2', '문2-우-닫힘.png', '문2-우-열림.png', room3)
+room2.door2.resize(120)
+room2.door2.locate(1000, 305)
+room2.door2.hide()
+
+room2.keypad1 = new Keypad(room2, 'keypad1', '키패드-우.png', '1234', function(){
+	printMessage('스르륵 문이 보인다')
+	room2.door2.show()
+})
+room2.keypad1.resize(20)
+room2.keypad1.locate(920, 250)
+
+// onClick 함수를 재정의할 수도 있다
+room2.keypad1.onClick = function(){
+	printMessage('1234')
+	showKeypad('number', this.password, this.callback)
+}
+
+////*** 세번째 방 ***////
+room3.door1 = new Door(room3, 'door1', '문2-좌-닫힘.png', '문2-좌-열림.png', room2)
+room3.door1.resize(120)
+room3.door1.locate(300, 320)
+
+room3.door2 = new Door(room3, 'door2', '문3-우-닫힘.png', '문3-우-열림.png', storage)
+room3.door2.resize(120)
+room3.door2.locate(1000, 320)
+room3.door2.lock()
+
+room3.lock1 = new DoorLock(room3, 'lock1', '키패드-우.png', '1234', room3.door2, '철커덕')
+room3.lock1.resize(20)
+room3.lock1.locate(920, 250)
+room3.lock1.onClick = function(){
+	printMessage('1234')
+	showKeypad('number', this.password, this.callback)
+}
+
+////*** 창고 ***////
+storage.door = new Direction(storage, 'door', '투명.png', lab)
+storage.door.resize(280)
+storage.door.locate(690,350)
+storage.door.lock()
+storage.door.onClick = function(){
+	if(storage.lock.isLocked()){
+		printMessage('닫혀있다\n아무래도 저 자물쇠를 열어야 하는 모양이다')
+	} else if(!storage.lock.isLocked()){
+		Game.move(this.connectedTo)
 	}
 }
 
-roomLight = true // 플래그 변수
-room.remote = room.createObject("remote", "리모컨.png")
-room.remote.setWidth(70)
-room.locateObject(room.remote, 800, 550)
-room.remote.onClick = function(){
-	if(roomLight){
-		room.setRoomLight(0.5)
-		roomLight = false
-	} else{
-		room.setRoomLight(1)
-		roomLight = true
+storage.direction = new Direction(storage, 'direction', '아래.png', room3)
+storage.direction.resize(23)
+storage.direction.locate(640, 697)
+
+storage.lock = new openclose(storage, 'lock', '자물쇠-잠김.png', '자물쇠-열림.png')
+storage.lock.resize(40)
+storage.lock.locate(815, 490)
+storage.lock.lock()
+storage.lock.onClick = function(){
+	if(room1.key.isHanded()){
+		this.id.setSprite(this.openedImage)
+		this.id.unlock()
+	} else if(!room1.key.isHanded()){
+		printMessage('이 자물쇠.. 혹시 아까 그거로?')
 	}
 }
 
-room2.door2 = room2.createObject("door2", "문3-좌-닫힘.png")
-room2.door2.setWidth(136)
-room2.locateObject(room2.door2, 170, 335)
-
-room2.closet = room2.createObject("closet", "옷장-1-닫힘.png")
-room2.closet.setWidth(300)
-room2.locateObject(room2.closet, 250,305)
-
-room2.closet.move = true // 플래그 변수 
-room2.closet.onDrag = function(direction){ // 드래그 모션 direction - Up, Down, Left, Right
-	if(direction == "Right" && room2.closet.move){ // 오른쪽으로 드래그했으면
-		printMessage("옷장을 밀어버렸다!")
-		room2.closet.moveX(200) // X방향으로 200 이동
-		room2.closet.moveY(-40) // Y방향으로 -40 이동
-		room2.closet.move = false // 이후에는 더이상 움직이지 않음
-	} else{
-		printMessage("열리지 않는다")
-	}
+////*** 실험실 ***////
+lab.setRoomLight(0.01) // 방 밝기를 0.01로 변경
+lab.event = new Object(lab, 'event', '투명.png')
+lab.event.resize(1000)
+lab.event.locate(640, 420)
+lab.event.onClick = function(){
+	playSound('전기충격.wav')
+	printMessage('지지지지직!!')
+	Game.end()
 }
 
-room2.door2.onClick = function() { // door2를 클릭했을 때
-	if(room2.door2.isClosed()){ // door2가 closed 상태이면
-		room2.door2.open() // door2를 open
-	} else if (room2.door2.isOpened()){ // door2가 opened 상태이면
-		game.clear() // 게임 클리어
-	} else {
-		// 아무것도 하지 않는다
-	}
-}
 
-game.start(room) // 게임시작
-printMessage("방탈출에 오신 것을 환영합니다!") // 환영 메시지 출력
+Game.start(room1, '아아, 들리십니까?\n\n들리십니까??!\n\n(일부 효과음이 있습니다)')
